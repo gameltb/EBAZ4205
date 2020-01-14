435ab2b5be22203e705dbe905691e5dc0be201a6
gameltb <gamegccltb@gmail.com>
2020-01-14 16:21:27 +0800

    update gitignore

---
+++ EBAZ4205/EBAZ4205.srcs/sources_1/bd/EBAZ4205/hdl/EBAZ4205_wrapper.v
@@ -0,0 +1,142 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+//Date        : Tue Jan 14 16:20:30 2020
+//Host        : ltb-Standard-PC-Q35-ICH9-2009 running 64-bit Ubuntu 18.04.3 LTS
+//Command     : generate_target EBAZ4205_wrapper.bd
+//Design      : EBAZ4205_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module EBAZ4205_wrapper
+   (DDR_0_addr,
+    DDR_0_ba,
+    DDR_0_cas_n,
+    DDR_0_ck_n,
+    DDR_0_ck_p,
+    DDR_0_cke,
+    DDR_0_cs_n,
+    DDR_0_dm,
+    DDR_0_dq,
+    DDR_0_dqs_n,
+    DDR_0_dqs_p,
+    DDR_0_odt,
+    DDR_0_ras_n,
+    DDR_0_reset_n,
+    DDR_0_we_n,
+    ENET0_GMII_RXD_0,
+    ENET0_GMII_RX_CLK_0,
+    ENET0_GMII_RX_DV_0,
+    ENET0_GMII_TXD_0,
+    ENET0_GMII_TX_CLK_0,
+    ENET0_GMII_TX_EN_0,
+    FIXED_IO_0_ddr_vrn,
+    FIXED_IO_0_ddr_vrp,
+    FIXED_IO_0_mio,
+    FIXED_IO_0_ps_clk,
+    FIXED_IO_0_ps_porb,
+    FIXED_IO_0_ps_srstb,
+    MDIO_ETHERNET_0_0_mdc,
+    MDIO_ETHERNET_0_0_mdio_io);
+  inout [14:0]DDR_0_addr;
+  inout [2:0]DDR_0_ba;
+  inout DDR_0_cas_n;
+  inout DDR_0_ck_n;
+  inout DDR_0_ck_p;
+  inout DDR_0_cke;
+  inout DDR_0_cs_n;
+  inout [3:0]DDR_0_dm;
+  inout [31:0]DDR_0_dq;
+  inout [3:0]DDR_0_dqs_n;
+  inout [3:0]DDR_0_dqs_p;
+  inout DDR_0_odt;
+  inout DDR_0_ras_n;
+  inout DDR_0_reset_n;
+  inout DDR_0_we_n;
+  input [3:0]ENET0_GMII_RXD_0;
+  input ENET0_GMII_RX_CLK_0;
+  input ENET0_GMII_RX_DV_0;
+  output [3:0]ENET0_GMII_TXD_0;
+  input ENET0_GMII_TX_CLK_0;
+  output [0:0]ENET0_GMII_TX_EN_0;
+  inout FIXED_IO_0_ddr_vrn;
+  inout FIXED_IO_0_ddr_vrp;
+  inout [53:0]FIXED_IO_0_mio;
+  inout FIXED_IO_0_ps_clk;
+  inout FIXED_IO_0_ps_porb;
+  inout FIXED_IO_0_ps_srstb;
+  output MDIO_ETHERNET_0_0_mdc;
+  inout MDIO_ETHERNET_0_0_mdio_io;
+
+  wire [14:0]DDR_0_addr;
+  wire [2:0]DDR_0_ba;
+  wire DDR_0_cas_n;
+  wire DDR_0_ck_n;
+  wire DDR_0_ck_p;
+  wire DDR_0_cke;
+  wire DDR_0_cs_n;
+  wire [3:0]DDR_0_dm;
+  wire [31:0]DDR_0_dq;
+  wire [3:0]DDR_0_dqs_n;
+  wire [3:0]DDR_0_dqs_p;
+  wire DDR_0_odt;
+  wire DDR_0_ras_n;
+  wire DDR_0_reset_n;
+  wire DDR_0_we_n;
+  wire [3:0]ENET0_GMII_RXD_0;
+  wire ENET0_GMII_RX_CLK_0;
+  wire ENET0_GMII_RX_DV_0;
+  wire [3:0]ENET0_GMII_TXD_0;
+  wire ENET0_GMII_TX_CLK_0;
+  wire [0:0]ENET0_GMII_TX_EN_0;
+  wire FIXED_IO_0_ddr_vrn;
+  wire FIXED_IO_0_ddr_vrp;
+  wire [53:0]FIXED_IO_0_mio;
+  wire FIXED_IO_0_ps_clk;
+  wire FIXED_IO_0_ps_porb;
+  wire FIXED_IO_0_ps_srstb;
+  wire MDIO_ETHERNET_0_0_mdc;
+  wire MDIO_ETHERNET_0_0_mdio_i;
+  wire MDIO_ETHERNET_0_0_mdio_io;
+  wire MDIO_ETHERNET_0_0_mdio_o;
+  wire MDIO_ETHERNET_0_0_mdio_t;
+
+  EBAZ4205 EBAZ4205_i
+       (.DDR_0_addr(DDR_0_addr),
+        .DDR_0_ba(DDR_0_ba),
+        .DDR_0_cas_n(DDR_0_cas_n),
+        .DDR_0_ck_n(DDR_0_ck_n),
+        .DDR_0_ck_p(DDR_0_ck_p),
+        .DDR_0_cke(DDR_0_cke),
+        .DDR_0_cs_n(DDR_0_cs_n),
+        .DDR_0_dm(DDR_0_dm),
+        .DDR_0_dq(DDR_0_dq),
+        .DDR_0_dqs_n(DDR_0_dqs_n),
+        .DDR_0_dqs_p(DDR_0_dqs_p),
+        .DDR_0_odt(DDR_0_odt),
+        .DDR_0_ras_n(DDR_0_ras_n),
+        .DDR_0_reset_n(DDR_0_reset_n),
+        .DDR_0_we_n(DDR_0_we_n),
+        .ENET0_GMII_RXD_0(ENET0_GMII_RXD_0),
+        .ENET0_GMII_RX_CLK_0(ENET0_GMII_RX_CLK_0),
+        .ENET0_GMII_RX_DV_0(ENET0_GMII_RX_DV_0),
+        .ENET0_GMII_TXD_0(ENET0_GMII_TXD_0),
+        .ENET0_GMII_TX_CLK_0(ENET0_GMII_TX_CLK_0),
+        .ENET0_GMII_TX_EN_0(ENET0_GMII_TX_EN_0),
+        .FIXED_IO_0_ddr_vrn(FIXED_IO_0_ddr_vrn),
+        .FIXED_IO_0_ddr_vrp(FIXED_IO_0_ddr_vrp),
+        .FIXED_IO_0_mio(FIXED_IO_0_mio),
+        .FIXED_IO_0_ps_clk(FIXED_IO_0_ps_clk),
+        .FIXED_IO_0_ps_porb(FIXED_IO_0_ps_porb),
+        .FIXED_IO_0_ps_srstb(FIXED_IO_0_ps_srstb),
+        .MDIO_ETHERNET_0_0_mdc(MDIO_ETHERNET_0_0_mdc),
+        .MDIO_ETHERNET_0_0_mdio_i(MDIO_ETHERNET_0_0_mdio_i),
+        .MDIO_ETHERNET_0_0_mdio_o(MDIO_ETHERNET_0_0_mdio_o),
+        .MDIO_ETHERNET_0_0_mdio_t(MDIO_ETHERNET_0_0_mdio_t));
+  IOBUF MDIO_ETHERNET_0_0_mdio_iobuf
+       (.I(MDIO_ETHERNET_0_0_mdio_o),
+        .IO(MDIO_ETHERNET_0_0_mdio_io),
+        .O(MDIO_ETHERNET_0_0_mdio_i),
+        .T(MDIO_ETHERNET_0_0_mdio_t));
+endmodule
